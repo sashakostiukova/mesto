ae6ab54c2bf25a060139c9e451fbf413e9706638
Sasha Kostiukova <sashakostiukova@yandex.ru>
2023-07-04 00:30:36 +0300

    fix: исправить скрипт после ревью

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Дагестан',
+    link: './images/places/dagestan.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/places/elbrus.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/places/dombay.jpg'
+  },
+  {
+    name: 'Башкортостан',
+    link: './images/places/bashkortostan.jpg'
+  },
+  {
+    name: 'Остров Итуруп',
+    link: './images/places/iturup.jpg'
+  },
+  {
+    name: 'Карачаево-Черкессия',
+    link: './images/places/karachayevsk.jpg'
+  }
+]; 
